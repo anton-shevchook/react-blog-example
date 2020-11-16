922ecaff6d5b630a3eb717335b59e3a8ff1adda2
Anton Shevchuk <anton.shevchuk@skipthedishes.ca>
2020-11-16 03:54:37 -0600

    Updates

---
+++ precache-manifest.abb1f60f067822bf5834b5d24cd06ab7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b6ba3266abf87f79ced24228256a8d",
+    "url": "/react-blog-example/index.html"
+  },
+  {
+    "revision": "54ab114adcd8e71f75b5",
+    "url": "/react-blog-example/static/css/main.9ca1e34c.chunk.css"
+  },
+  {
+    "revision": "969a79751f5849b5b39a",
+    "url": "/react-blog-example/static/js/2.aa4f1ad7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-blog-example/static/js/2.aa4f1ad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ab114adcd8e71f75b5",
+    "url": "/react-blog-example/static/js/main.af068492.chunk.js"
+  },
+  {
+    "revision": "b8f61fc2fdc4c274ec57",
+    "url": "/react-blog-example/static/js/runtime-main.271389af.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-blog-example/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d522b0ff9106c05de45fb382c895cb5.js"
+  "/react-blog-example/precache-manifest.abb1f60f067822bf5834b5d24cd06ab7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-blog-example/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
