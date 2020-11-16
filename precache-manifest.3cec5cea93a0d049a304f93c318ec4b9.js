5d952a80502bcf64a87829728467d0d3bf2822e9
Anton Shevchuk <anton.shevchuk@skipthedishes.ca>
2020-11-16 01:29:00 -0600

    Updates

---
+++ precache-manifest.3cec5cea93a0d049a304f93c318ec4b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb993a14f90ade8eaef16f1cded8997",
+    "url": "/react-blog-example/index.html"
+  },
+  {
+    "revision": "be0f68136b5dc04e22be",
+    "url": "/react-blog-example/static/css/main.26d46b87.chunk.css"
+  },
+  {
+    "revision": "969a79751f5849b5b39a",
+    "url": "/react-blog-example/static/js/2.aa4f1ad7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-blog-example/static/js/2.aa4f1ad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0f68136b5dc04e22be",
+    "url": "/react-blog-example/static/js/main.08350f02.chunk.js"
+  },
+  {
+    "revision": "b8f61fc2fdc4c274ec57",
+    "url": "/react-blog-example/static/js/runtime-main.271389af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-blog-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
